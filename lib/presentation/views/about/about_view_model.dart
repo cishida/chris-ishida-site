83dbd6dbf6c9e7ac8a8e73531c9294f92dc4befd
Christopher Ishida <christopherishida@Christophers-MacBook-Pro.local>
2022-08-02 15:00:02 -0700

    Adding page skeletons.

---
+++ lib/presentation/views/about/about_view_model.dart
@@ -0,0 +1,7 @@
+import 'package:stacked/stacked.dart';
+
+class AboutViewModel extends BaseViewModel {
+  final String _title = 'About View';
+
+  String get title => _title;
+}
---
+++ lib/presentation/views/products/products_view_model.dart
@@ -0,0 +1,7 @@
+import 'package:stacked/stacked.dart';
+
+class ProductsViewModel extends BaseViewModel {
+  final String _title = 'Products View';
+
+  String get title => _title;
+}
